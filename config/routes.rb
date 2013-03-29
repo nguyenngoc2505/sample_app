d1e5f7714b208d550fb5395f258f24e3b2826ff7
Ngoc Nguyen <nguyenngoc2505@gmail.com>
2013-03-29 09:29:57 +0700

    Finish sign-in-out

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ SampleApp::Application.routes.draw do
  
 
   resources :users
-root to: 'static_pages#home'
- match '/',to: 'static_pages#home'
- match '/signup', to: 'users#new'
+  resources :sessions, only: [:new, :create, :destroy]
+  root to: 'static_pages#home'
+  match '/',to: 'static_pages#home'
+  match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   get 'static_pages/home'
   match '/help', to: 'static_pages#help'
   get 'static_pages/help'
