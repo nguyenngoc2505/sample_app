539d7551eb330155e37a2dc73adb69f56322deab
Ngoc Nguyen <nguyenngoc2505@gmail.com>
2013-04-03 11:24:28 +0700

    add user micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
@@ -22,6 +23,9 @@ class User < ActiveRecord::Base
   # lenh tren tuong duong lenh : validates(:name, presence: true)
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
   private
   def create_remember_token
     self.remember_token = SecureRandom.urlsafe_base64
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,10 +3,24 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Home page" do
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}",text: item.content)
+        end
+      end
+    end
 
     it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+      page.should have_selector('h1', :text => 'Welcome to the Sample App')
     end
 
     it "should have the title 'Home'" do
